f91d407b05aaca56a1a2ca79d50391f7e2adf52e
Adnan Muhib <adnan.muhib@rocketmail.com>
2017-11-23 01:04:18 +0500

    Add HelloWorld Controller
    Added New Welcome Action in the Controller

---
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is <b>default</b> Action";
+        }
+        // Post: Parameters
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Name: " + name + ", numTimes is: " + ID );
+        }
+    }
+}
