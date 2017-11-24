f4dc263d7c2813ea2ed0e777ca46b3a4b9eeffe2
Adnan Muhib <adnan.muhib@rocketmail.com>
2017-11-25 00:50:20 +0500

    Add Movie Model

---
+++ MVCMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public Decimal Price { get; set; }
+    }
+    public class MovieDBContext:DbContext {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
